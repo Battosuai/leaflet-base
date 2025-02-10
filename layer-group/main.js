f4fcdd493bd3cb596ddd1031d16bc78b4f633aae
MD. Asifur Rahman <imdjiban@gmail.com>
2025-02-11 01:07:09 +0600

    layer group done

--- layer-group/main.js
+++ layer-group/main.js
@@ -1,44 +1,61 @@
 function main() {
-    const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap',
-    });
+  const osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: "© OpenStreetMap",
+  });
 
-    const osmHOT = L.tileLayer(
-        'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-        {
-            maxZoom: 19,
-            attribution:
-                '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France',
-        }
-    );
+  const osmHOT = L.tileLayer(
+    "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+    {
+      maxZoom: 19,
+      attribution:
+        "© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France",
+    }
+  );
 
-    const littleton = L.marker([39.61, -105.02])
-        .bindPopup('This is Littleton, CO.'),
-        denver = L.marker([39.74, -104.99])
-        .bindPopup('This is Denver, CO.'),
-        aurora = L.marker([39.73, -104.8])
-        .bindPopup('This is Aurora, CO.'),
-        golden = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+  const littleton = L.marker([39.61, -105.02]).bindPopup(
+      "This is Littleton, CO."
+    ),
+    denver = L.marker([39.74, -104.99]).bindPopup("This is Denver, CO."),
+    aurora = L.marker([39.73, -104.8]).bindPopup("This is Aurora, CO."),
+    golden = L.marker([39.77, -105.23]).bindPopup("This is Golden, CO.");
 
-    const cities = L.layerGroup([littleton, denver, aurora, golden]);
+  const cities = L.layerGroup([littleton, denver, aurora, golden]);
 
-    const map = L.map('map', {
-        center: [39.73, -104.99],
-        zoom: 10,
-        layers: [osm, cities],
-    });
+  const map = L.map("map", {
+    center: [39.73, -104.99],
+    zoom: 10,
+    layers: [osm, cities],
+  });
 
-    const baseMaps = {
-        OpenStreetMap: osm,
-        'OpenStreetMap.HOT': osmHOT,
-    };
+  const baseMaps = {
+    OpenStreetMap: osm,
+    "<span style='color: red'>OpenStreetMap.HOT</span>": osmHOT,
+  };
 
-    const overlayMaps = {
-        Cities: cities,
-    };
+  const overlayMaps = {
+    Cities: cities,
+  };
 
-    const layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+  const layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+  const crownHill = L.marker([39.75, -105.09]).bindPopup(
+      "This is Crown Hill Park."
+    ),
+    rubyHill = L.marker([39.68, -105.0]).bindPopup("This is Ruby Hill Park.");
+
+  const parks = L.layerGroup([crownHill, rubyHill]);
+  const openTopoMap = L.tileLayer(
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+    {
+      maxZoom: 19,
+      attribution:
+        "Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)",
+    }
+  );
+
+  layerControl.addBaseLayer(openTopoMap, "OpenTopoMap");
+  layerControl.addOverlay(parks, "Parks");
 }
 
-window.addEventListener('load', main);
+window.addEventListener("load", main);
